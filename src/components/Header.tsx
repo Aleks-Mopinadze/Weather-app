306d9fdd87b4ea5f15a01b4fcfa26cd462f8c65e
User260901 <alexmofinadze0@gmail.com>
2026-02-20 07:17:07 +0400

    fixed search and theme button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,12 +11,14 @@ const Header = ()=> {
     return(
         <header className='sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur py-2 supports-[backdrop-filter]:bg-background/60'>
             <div className='container mx-auto flex h-16 items-center justify-between px-4'>
-                <Link to={'/'}>
+                <Link to={'/'} >
                     <img src={isDark ? '/logo.png' : '/logo2.png'} alt="Logo" className='h-14'/>
                 </Link>
 
                 <div className='flex gap-4'>
-                    <CitySearch/>
+                    <div>
+                        <CitySearch/>
+                    </div>
 
                     <div onClick={()=> setTheme(isDark ? 'light' : 'dark')} className={`flex items-center cursor-pointer transition-transform duration-500  ${isDark ? 'rotate-180' : 'rotate-0'}`}>
                         {isDark ? <Sun className='h-6 w-6 text-yellow-500 rotate-0 transition-all'/> :  <Moon className='h-6 w-6 text-blue-500 rotate-0 transition-all'/> }
